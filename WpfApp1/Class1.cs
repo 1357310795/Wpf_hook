6cc5dfbc02a5d5c30e94c3afc8d3097798ce1fba
Mike Smith <1357310795@qq.com>
2021-03-22 13:13:33 +0800

    添加项目文件。

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Windows.Input;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Class1
+    {
+        #region 屏蔽WIN功能键
+        public delegate int HookProc(int nCode, int wParam, IntPtr lParam);
+        private static int hHook = 0;
+        public const int WH_KEYBOARD_LL = 13;
+
+        //LowLevel键盘截获，如果是WH_KEYBOARD＝2，并不能对系统键盘截取，会在你截取之前获得键盘。 
+        private static HookProc KeyBoardHookProcedure;
+        //键盘Hook结构函数 
+        [StructLayout(LayoutKind.Sequential)]
+        public class KeyBoardHookStruct
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        //设置钩子 
+        [DllImport("user32.dll")]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        //抽掉钩子 
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        [DllImport("user32.dll")]
+        //调用下一个钩子 
+        public static extern int CallNextHookEx(int idHook, int nCode, int wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        public static extern int GetCurrentThreadId();
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string name);
+
+        public static void Hook_Start()
+        {
+            // 安装键盘钩子 
+            if (hHook == 0)
+            {
+                KeyBoardHookProcedure = new HookProc(KeyBoardHookProc);
+                hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyBoardHookProcedure,
+                        GetModuleHandle(Process.GetCurrentProcess().MainModule.ModuleName), 0);
+                //如果设置钩子失败. 
+                if (hHook == 0)
+                {
+                    Hook_Clear();
+                }
+            }
+        }
+
+        //取消钩子事件 
+        public static void Hook_Clear()
+        {
+            bool retKeyboard = true;
+            if (hHook != 0)
+            {
+                retKeyboard = UnhookWindowsHookEx(hHook);
+                hHook = 0;
+            }
+            //如果去掉钩子失败. 
+            if (!retKeyboard) throw new Exception("UnhookWindowsHookEx failed.");
+        }
+
+        //这里可以添加自己想要的信息处理 
+        private static int KeyBoardHookProc(int nCode, int wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                KeyBoardHookStruct kbh = (KeyBoardHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyBoardHookStruct));
+                //if (kbh.vkCode == (int)Keys.Escape && (int)ModifierKeys == (int)Keys.Control) //截获Ctrl+Esc 
+                //{
+                //    return 1;
+                //}
+                //Console.WriteLine(kbh.vkCode);
+                if ((Keyboard.Modifiers == ModifierKeys.Shift) && (kbh.vkCode==116))
+                {
+                    Console.WriteLine("ShiftF5");
+                    Thread thread = new Thread(new ThreadStart(lockwindows));
+                    thread.Start();                                                           //启动线程
+                    
+                }
+            }
+
+            return CallNextHookEx(hHook, nCode, wParam, lParam);
+        }
+
+        public static void lockwindows()
+        {
+            Process.Start("rundll32.exe", "user32.dll,LockWorkStation");
+        }
+        public static void TaskMgrLocking(bool bLock)
+        {
+            if (bLock)
+            {
+                RegistryKey r = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true);
+                if (r is null)
+                    r = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+                //r.SetValue("DisableTaskmgr", "1"); //屏蔽任务管理器 
+            }
+            else
+            {
+                RegistryKey r = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true);
+                //r.SetValue("DisableTaskmgr", "0");
+                //Registry.CurrentUser.DeleteSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\DisableTaskmgr");
+            }
+        }
+        #endregion
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random rd = new Random();
+        int[] a = new int[5];
+        int n, err;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ScreenUnlock_AfterDraw(object sender, RoutedEventArgs e)
+        {
+            if (check()) ok(); else { err++; ScreenUnlock1.SelectedColor = new SolidColorBrush(Colors.Red); }
+            if (err == 3) Init();
+        }
+
+        private void ok()
+        {
+            ScreenUnlock1.SelectedColor = new SolidColorBrush(Colors.Green);
+            Class1.TaskMgrLocking(false);
+            Class1.Hook_Clear();
+            Application.Current.Shutdown();
+        }
+
+        private bool check()
+        {
+            string s1 = "", s = "";
+            for (int i = 1; i <= n; i++)
+                s1 = s1 + (a[i] % 3).ToString();
+            Console.WriteLine(s1);
+            foreach (int i in ScreenUnlock1.Points)
+                s += ((i - 1) % 3).ToString();
+            Console.WriteLine(s);
+            return s == s1;
+        }
+
+        private int Ran(int l, int r)
+        {
+            return rd.Next(l, r + 1);
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Init();
+            Class1.TaskMgrLocking(true);
+            Class1.Hook_Start();
+        }
+
+        private void Init()
+        {
+            err = 0;
+            string s = "";
+            n = Ran(2, 4);
+
+            for (int i = 1; i <= n; i++)
+            {
+                a[i] = Ran(0, 9);
+                if ((i == 4) && (a[4] % 3 == a[2] % 3))
+                    i--;
+            }
+            int[] b = new int[15];
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 1 && (i + 1) / 2 <= n)
+                    b[i] = a[(i + 1) / 2];
+                else
+                    if (i == 10)
+                    b[i] = n;
+                else
+                    b[i] = Ran(0, 9);
+                s = s + b[i].ToString();
+            }
+            text1.Text = s;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Class1.TaskMgrLocking(true);
+            Class1.Hook_Start();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Class1.TaskMgrLocking(false);
+            Class1.Hook_Clear();
+        }
+    }
+}
